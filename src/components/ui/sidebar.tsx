98751d20f2992fb2afbe9fccc35161dfd43605dc
Manorama <m3361555@gmail.com>
2025-07-25 07:58:01 +0000

    expand the complete page when the sidebar reduce

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -344,12 +344,24 @@ const SidebarInset = React.forwardRef<
   HTMLDivElement,
   React.ComponentProps<"main">
 >(({ className, ...props }, ref) => {
+  const { state, isMobile } = useSidebar();
+  
+  if (isMobile) {
+    return (
+       <main
+        ref={ref}
+        className={cn("relative flex min-h-svh flex-1 flex-col bg-background", className)}
+        {...props}
+      />
+    )
+  }
+
   return (
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background",
-        "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
+        "relative flex min-h-svh flex-1 flex-col bg-background transition-[margin-left] duration-200",
+        state === 'expanded' ? 'md:ml-[16rem]' : 'md:ml-[3.5rem]',
         className
       )}
       {...props}

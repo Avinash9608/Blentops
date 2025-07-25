bc11c4dbc07eb709857e24594f8ae0e298547f27
Manorama <m3361555@gmail.com>
2025-07-25 07:14:19 +0000

    Write only  Blentops remove admin <link rel="preconnect" href="https://

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -30,8 +30,8 @@ export default function LoginPage() {
                 </svg>
               </div>
               <CardHeader className="text-center p-0 text-white">
-                <CardTitle className="text-2xl font-bold">Blentops Admin</CardTitle>
-                <CardDescription className="text-white/80">
+                <CardTitle className="text-4xl font-serif">Blentops</CardTitle>
+                <CardDescription className="text-white/80 pt-2">
                   Welcome back! Please sign in to continue.
                 </CardDescription>
               </CardHeader>

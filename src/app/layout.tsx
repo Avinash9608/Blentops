23646f22a781d80eacf726568f62aa2b22f7a127
Manorama <m3361555@gmail.com>
2025-07-25 07:15:42 +0000

    font not apply

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/hooks/use-auth';
 
 export const metadata: Metadata = {
-  title: 'Blentos Admin Console',
+  title: 'Blentops',
   description: 'Admin panel for managing the Blentos website.',
 };
 
